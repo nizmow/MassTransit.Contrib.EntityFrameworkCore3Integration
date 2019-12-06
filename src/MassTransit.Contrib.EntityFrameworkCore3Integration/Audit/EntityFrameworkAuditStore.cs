360761cdccbac022c6489dc4ae5b1270d9d9a831
Neil Houghton <n.houghton@payvision.com>
2019-12-06 14:54:03 +0100

    Mostly work on license files, phew

--- src/MassTransit.Contrib.EntityFrameworkCore3Integration/Audit/EntityFrameworkAuditStore.cs
+++ src/MassTransit.Contrib.EntityFrameworkCore3Integration/Audit/EntityFrameworkAuditStore.cs
@@ -1,4 +1,5 @@
-﻿// Copyright 2007-2017 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+﻿// Copyright 2007-2019 Chris Patterson, Dru Sellers, Travis Smith, et. al.
+// Modifications copyright 2019 Neil Houghton.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"); you may not use
 // this file except in compliance with the License. You may obtain a copy of the
